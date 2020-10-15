ba9b059fa35e42d8d4be70b6ae20615c58f55443
riabow <63100171+riabov-vladimir@users.noreply.github.com>
2020-10-15 13:41:00 +0300

    На проверку

--- app/models.py
+++ app/models.py
@@ -5,7 +5,6 @@ class Car(models.Model):
     brand = models.CharField(max_length=50)
     model = models.CharField(max_length=50)
 
-
     def __str__(self):
         return f'{self.brand} {self.model}'
 
